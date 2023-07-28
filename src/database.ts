37b70df9b1d854da063ff4dd1a000b30dd1f47fb
Daniel Leutwyler <daniel.leutwyler@gmx.ch>
2023-07-28 13:55:58 +0200

    npm run test now running on test DB

--- src/database.ts
+++ src/database.ts
@@ -3,14 +3,39 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const { POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
-  process.env;
+const {
+  POSTGRES_HOST,
+  POSTGRES_DEV_DB,
+  POSTGRES_TEST_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  ENV,
+} = process.env;
 
-const Client = new Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
-});
+let Client: Pool;
+
+console.log(`ENV=[${ENV?.trim()}]`);
+
+if (ENV?.trim() == 'test') {
+  console.log(
+    `IM TEST MODUS - database.ts: ENV=[${ENV}] | database = ${POSTGRES_TEST_DB}`
+  );
+  Client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+} else {
+  console.log(
+    `NICHT IM TEST MODUS: database.ts: ENV=${ENV} | database = ${POSTGRES_DEV_DB}`
+  );
+  Client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DEV_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+  });
+}
 
 export default Client;
